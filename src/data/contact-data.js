a41342653a72d2aefb6f347d66f8b64cfa3b13b2
pushpraj45 <pushprajdubey20@gmail.com>
2024-02-09 17:59:11 +0530

    main

---
+++ src/data/contact-data.js
@@ -0,0 +1,27 @@
+import {
+  BriefcaseIcon,
+  ChartBarIcon,
+  PlayIcon,
+} from "@heroicons/react/24/solid";
+
+export const contactData = [
+  {
+    title: "Mobile Responsiveness",
+    icon: BriefcaseIcon,
+    description:
+      "Mobile-friendly design to ensure access and functionality on a variety of devices.",
+  },
+  {
+    title: "User-Friendly Questionnaire",
+    icon: ChartBarIcon,
+    description:
+      "A user-friendly questionnaire that gathers relevant information about the child, including their behavior, development, and other key indicators.",
+  },
+  {
+    title: "Real-time Assessment",
+    icon: PlayIcon,
+    description:"The ability to provide real-time feedback to parents, caregivers, or users about the risk or likelihood of disorders such as ADHD, Dyslexia, Autism, etc.",
+  },
+];
+
+export default contactData;
---
+++ src/data/features-data.js
@@ -0,0 +1,29 @@
+import {
+  ChatBubbleBottomCenterTextIcon,
+} from "@heroicons/react/24/solid";
+
+export const featuresData = [
+  {
+    color: "gray",
+    title: "ADHD",
+    icon: ChatBubbleBottomCenterTextIcon,
+    description:
+      "Explore the world of ADHD with insights into symptoms, causes, and effective coping strategies for improved focus.",
+  },
+  {
+    color: "gray",
+    title: "Autism",
+    icon: ChatBubbleBottomCenterTextIcon,
+    description:
+      "Gain a comprehensive understanding of autism through an overview of its characteristics and manifestations.",
+  },
+  {
+    color: "gray",
+    title: "Dyslexia",
+    icon: ChatBubbleBottomCenterTextIcon,
+    description:
+      "Decode the challenges of dyslexia, uncovering insights into causes and symptoms.",
+  },
+];
+
+export default featuresData;
